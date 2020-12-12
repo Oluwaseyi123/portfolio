6003a598c2accdec147a55966596174cb9a19017
Oluwaseyi Aimudo <oluwaseyiaimudo@Oluwaseyis-MacBook-Pro.local>
2020-12-12 11:08:10 +0000

    commando

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const toggleIcon = document.querySelector('.toggle-icon')
 const toggle = document.querySelector('.toggle')
 toggleIcon.addEventListener('click', nightMode)
 let images = document.querySelectorAll('img')
+const svg = document.querySelector('svg')
 let icons = document.querySelectorAll('i')
 
 console.log(innerWidth)
@@ -12,17 +13,38 @@ function nightMode(){
         console.log(image)
         image.classList.toggle('body-night')
     })
-    // icons.forEach((icon) => {
-    //     console.log(icon)
-    //     icon.classList.toggle('body-night')
-    // })
     document.documentElement.classList.toggle('body-night')
     toggleIcon.classList.toggle('night')
+    svg.classList.toggle('body-night')
+
     console.log('night mode')
-
-
 }
+ 
+const fadeIn = document.querySelectorAll('.fade-in')
+const appearOptions = { 
+  threshold: 0.2,
+  rootMargin: "0px 0px -5px 0px"
+}
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+  entries.forEach(entry => {
+    if(!entry.isIntersecting){
+      return;
+    }else{
+      entry.target.classList.add('active')
+      appearOnScroll.unobserve(entry.target)
+    }
+  })
+}, appearOptions)
 
-// html[theme='dark-mode '] {
-//     filter: invert(1) hue-rotate(180deg);
-// }
+fadeIn.forEach(fade => {
+appearOnScroll.observe(fade)
+})
+
+const navIcon = document.querySelector('.nav-icon')
+navIcon.addEventListener('click', showNav)
+
+function showNav(e) {
+  
+  console.log(navIcon.childNodes)
+ 
+}
